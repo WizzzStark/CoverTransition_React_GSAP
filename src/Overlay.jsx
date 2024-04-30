6f7033791fe0bea84d593207dd033de9110f1cb2
WizzzStark <wizzzstk@gmail.com>
2024-04-30 19:56:56 +0200

    Overlay component

---
+++ src/Overlay.jsx
@@ -0,0 +1,11 @@
+function Overlay() {
+    return (
+        <div className="overlay">
+            <div className="overlay__row"></div>
+            <div className="overlay__row"></div>
+        </div>
+
+    )
+}
+
+export default Overlay;
