232ecf70a5ee16237a5dc226a7181992a7deea7e
WizzzStark <wizzzstk@gmail.com>
2024-04-30 19:56:00 +0200

    Main component and styles

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Frame from './Frame';
+import Content from './Content';
+
+function App() {
+  return (
+    <main>
+      <Frame />
+      <Content />
+    </main>
+  );
+}
+
+export default App;
